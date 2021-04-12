bfd130236d27615852dcfba1b5696c9a2e8d8421
KSJ <khb3221@naver.com>
2021-04-12 16:44:11 +0900

    feat: CRU 구현
    feat: CRU 구현

---
+++ webservice/src/main/java/com/edu/book/springboot/Controller/PostsApiController.java
@@ -0,0 +1,30 @@
+package com.edu.book.springboot.Controller;
+
+import com.edu.book.springboot.Controller.dto.PostsResponseDto;
+import com.edu.book.springboot.Controller.dto.PostsSaveRequestDto;
+import com.edu.book.springboot.Controller.dto.PostsUpdateRequestDto;
+import com.edu.book.springboot.service.PostsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
+    }
+}
