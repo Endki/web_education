c7e0a8f568d0587af389cad544ffb48ec3227229
KSJ <khb3221@naver.com>
2021-04-26 23:41:51 +0900

    feat: 머스테치 추가
    feat: 머스테치 추가

--- webservice/src/test/java/com/edu/book/springboot/domain/posts/PostsRepositoryTest.java
+++ webservice/src/test/java/com/edu/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -47,6 +48,31 @@ public class PostsRepositoryTest {
         Posts posts=postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+
+    }
+
+    @Test
+    public void BaseTimeEntity_등록(){
+        //given
+        LocalDateTime now= LocalDateTime.of(2019,6,4,0,0,0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        //when
+        List<Posts> postsList=postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>>>>>> createDate="+posts.getCreatedDate()+", modifiedDate="+ posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate().isAfter(now));
+        assertThat(posts.getModifiedDate()).isAfter(now);
+
+        //앞으로 추가될 엔티티들은 등록일/수정일로 고민할 필요가 없다.
 
     }
 
