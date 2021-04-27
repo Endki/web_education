75648863ebb28abb2822bc6cc383f69c9af26b04
KSJ <khb3221@naver.com>
2021-04-27 13:31:28 +0900

    feat: 머스테치 기초 화면 추가
    feat: 머스테치 기초 화면 추가

---
+++ webservice/src/main/java/com/edu/book/springboot/Controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.edu.book.springboot.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+}
