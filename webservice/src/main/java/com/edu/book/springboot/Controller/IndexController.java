a0c9de1121167fbb576148f1511c258263d607ea
KSJ <khb3221@naver.com>
2021-08-03 21:09:17 +0900

    #feat login 기능 구현
    #2021/08/03 endki
    
    로그인 기능 구현 (화면 수정, 테스트 진행 완료)

--- webservice/src/main/java/com/edu/book/springboot/Controller/IndexController.java
+++ webservice/src/main/java/com/edu/book/springboot/Controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.edu.book.springboot.Controller;
 
 import com.edu.book.springboot.Controller.dto.PostsResponseDto;
+import com.edu.book.springboot.config.auth.dto.SessionUser;
 import com.edu.book.springboot.service.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -8,15 +9,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts",postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null){
+            model.addAttribute("userName",user.getName());
+        }
         return "index";
     }
 
