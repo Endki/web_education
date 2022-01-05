fbe1b33b86af3e09bc2359822fdaa00a97667cd8
KSJ <khb3221@naver.com>
2022-01-05 20:21:30 +0900

    #fix header base 완성

--- webservice_front/mystyle/src/components/header.vue
+++ webservice_front/mystyle/src/components/header.vue
@@ -1,46 +1,118 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">Endki_portfolio</b-navbar-brand>
+    <b-container class="bv-example-row fluid">
+      <b-row>
+        <b-col>
+          <b-nav small align="left">
+            <b-nav-item link-classes="text-dark">즐겨찾기</b-nav-item>
+            <b-nav-item disabled>/</b-nav-item>
+            <b-nav-item disabled> 날짜</b-nav-item>
+          </b-nav>
+        </b-col>
 
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-col>
+          <b-nav small align="right">
+            <b-nav-item link-classes="text-dark">공지사항</b-nav-item>
 
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item href="#">Link</b-nav-item>
-          <b-nav-item href="#" disabled>Disabled</b-nav-item>
-        </b-navbar-nav>
+            <b-nav-item link-classes="text-dark">FAQ</b-nav-item>
 
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <b-nav-form>
-            <b-form-input
-              size="sm"
-              class="mr-sm-2"
-              placeholder="Search"
-            ></b-form-input>
-            <b-button size="sm" class="my-2 my-sm-0" type="submit"
-              >Search</b-button
-            >
-          </b-nav-form>
+            <b-nav-item link-classes="text-dark">출석부</b-nav-item>
 
-          <b-nav-item-dropdown text="Lang" right>
-            <b-dropdown-item href="#">EN</b-dropdown-item>
-            <b-dropdown-item href="#">ES</b-dropdown-item>
-            <b-dropdown-item href="#">RU</b-dropdown-item>
-            <b-dropdown-item href="#">FA</b-dropdown-item>
-          </b-nav-item-dropdown>
+            <b-nav-item link-classes="text-dark">닉네임</b-nav-item>
 
-          <b-nav-item-dropdown right>
-            <!-- Using 'button-content' slot -->
-            <template #button-content>
-              <em>User</em>
-            </template>
-            <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-          </b-nav-item-dropdown>
-        </b-navbar-nav>
-      </b-collapse>
+            <b-nav-item link-classes="text-dark">로그아웃</b-nav-item>
+          </b-nav></b-col
+        >
+      </b-row>
+    </b-container>
+
+    <!-- 사이에 사진 하나 들어가야댐 -->
+    <b-container>
+      <b-row>
+        <b-col></b-col>
+        <b-col class="header-logo" align-v="center">
+          <a href="#">
+            <!--<img
+          src="../assets/pinterest_profile_image.png"
+          alt="Endki_portfolio"
+          title=""
+          style="display: block; margin: 0 auto; width: 100px; height: 100px"
+        />  아래 b-img로 변경 -->
+            <b-img
+              v-bind="titleImage"
+              center
+              :src="require('../assets/pinterest_profile_image.png')"
+              alt="Center image"
+            ></b-img>
+          </a>
+          <span
+            class="header-desc"
+            style="
+              display: inline-block;
+              width: 100%;
+              text-align: center;
+              color: gray;
+            "
+          >
+            Endki-Portfolio
+          </span>
+        </b-col>
+        <b-col></b-col>
+      </b-row>
+    </b-container>
+
+    <!-- -->
+
+    <b-navbar toggleable="lg" type="dark" variant="dark" class="nav">
+      <b-container>
+        <b-navbar-brand href="#"
+          ><b-icon icon="house-door"></b-icon
+        ></b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item href="#">포트폴리오</b-nav-item>
+            <b-nav-item href="#">JavaScript 게임</b-nav-item>
+            <b-nav-item href="#">게시판</b-nav-item>
+          </b-navbar-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ms-auto">
+            <b-nav-item href="#"><b-icon icon="bell"></b-icon></b-nav-item>
+            <b-nav-item href="#">검색</b-nav-item>
+            <b-nav-item href="#">전체메뉴</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
     </b-navbar>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      titleImage: {
+        width: 100,
+        height: 100,
+      },
+      items: [
+        {
+          heading1: 'table cell',
+          heading2: 'table cell',
+          heading3: 'table cell',
+          heading4: 'table cell',
+          heading5: 'table cell',
+          heading6: 'table cell',
+          heading7: 'table cell',
+          heading8: 'table cell',
+          heading9: 'table cell',
+          heading10: 'table cell',
+          heading11: 'table cell',
+          heading12: 'table cell',
+        },
+      ],
+    };
+  },
+};
+</script>
+<style scoped></style>
--- webservice_front/mystyle/src/components/index.vue
+++ webservice_front/mystyle/src/components/index.vue
@@ -1,68 +1,69 @@
 <template>
-  <div id="thema_wrapper" class="wrapper ko">
-    <aside class="at-lnb">
-      <div class="at-container">
-        <!-- LNB Left -->
-        <div class="pull-left">
-          <ul>
-            <li><a href="javascript:;" id="favorite">즐겨찾기</a></li>
-            <li class="hidden">
-              <a href="https://gezip.net/rss/rss.php" target="_blank"
-                >RSS 구독</a
-              >
-            </li>
-            <!--홈짱-->
-            <li><a>01월 02일(일)</a></li>
-          </ul>
-        </div>
-        <!-- LNB Right -->
-        <div class="pull-right">
-          <ul>
-            <li>
-              <a href="https://gezip.net/bbs/board.php?bo_table=notice"
-                >공지사항</a
-              >
-            </li>
-            <!-- <li><a href="https://gezip.net/prison/prison.php">개펠다운</a></li> -->
-            <!-- <li>
+  <b-container>
+    <div id="thema_wrapper" class="wrapper ko">
+      <aside class="at-lnb">
+        <div class="at-container">
+          <!-- LNB Left -->
+          <div class="pull-left">
+            <ul>
+              <li><a href="javascript:;" id="favorite">즐겨찾기</a></li>
+              <li class="hidden">
+                <a href="https://gezip.net/rss/rss.php" target="_blank"
+                  >RSS 구독</a
+                >
+              </li>
+              <!--홈짱-->
+              <li><a>01월 02일(일)</a></li>
+            </ul>
+          </div>
+          <!-- LNB Right -->
+          <div class="pull-right">
+            <ul>
+              <li>
+                <a href="https://gezip.net/bbs/board.php?bo_table=notice"
+                  >공지사항</a
+                >
+              </li>
+              <!-- <li><a href="https://gezip.net/prison/prison.php">개펠다운</a></li> -->
+              <!-- <li>
               <a href="https://gezip.net/bbs/board.php?bo_table=suggestion"
                 >일름보게시판</a
               >
             </li> -->
-            <li>
-              <a href="https://gezip.net/bbs/board.php?bo_table=attendance"
-                >출석부</a
-              >
-            </li>
-            <!--홈짱-->
+              <li>
+                <a href="https://gezip.net/bbs/board.php?bo_table=attendance"
+                  >출석부</a
+                >
+              </li>
+              <!--홈짱-->
 
-            <li>
-              <a
-                href="javascript:;"
-                onclick="sidebar_open(&#39;sidebar-user&#39;);"
-                ><b>user-ID</b></a
-              >
-            </li>
-            <li class="sidebarLabel" style="display: none">
-              <a
-                href="javascript:;"
-                onclick="sidebar_open(&#39;sidebar-response&#39;);"
-              >
-                알림 <b class="orangered sidebarCount">0</b>
-              </a>
-            </li>
-            <li>
-              <a href="https://gezip.net/bbs/logout.php?url=%2F">로그아웃 </a>
-            </li>
-          </ul>
+              <li>
+                <a
+                  href="javascript:;"
+                  onclick="sidebar_open(&#39;sidebar-user&#39;);"
+                  ><b>user-ID</b></a
+                >
+              </li>
+              <li class="sidebarLabel" style="display: none">
+                <a
+                  href="javascript:;"
+                  onclick="sidebar_open(&#39;sidebar-response&#39;);"
+                >
+                  알림 <b class="orangered sidebarCount">0</b>
+                </a>
+              </li>
+              <li>
+                <a href="https://gezip.net/bbs/logout.php?url=%2F">로그아웃 </a>
+              </li>
+            </ul>
+          </div>
+          <div class="clearfix"></div>
         </div>
-        <div class="clearfix"></div>
-      </div>
-    </aside>
-    <header class="pc-header">
-      <div class="at-container">
-        <div class="header-logo">
-          <a href="https://gezip.net/">
+      </aside>
+      <header class="pc-header">
+        <div class="at-container">
+          <!-- <div class="header-logo">
+          <a href="#">
             <img
               src="../assets/pinterest_profile_image.png"
               alt="Endki_portfolio"
@@ -70,47 +71,48 @@
             />
           </a>
           <span class="header-desc"> Endki_portfolio </span>
+        </div>-->
         </div>
-      </div>
-      <div class="header-search">
-        <form
-          name="tsearch"
-          method="get"
-          onsubmit="return tsearch_submit(this);"
-          role="form"
-          class="form"
-        >
-          <input
-            type="hidden"
-            name="url"
-            value="https://gezip.net/bbs/search.php"
-          />
-          <div class="input-group input-group-sm">
+        <div class="header-search">
+          <form
+            name="tsearch"
+            method="get"
+            onsubmit="return tsearch_submit(this);"
+            role="form"
+            class="form"
+          >
             <input
-              type="text"
-              name="stx"
-              class="form-control input-sm"
-              value=""
+              type="hidden"
+              name="url"
+              value="https://gezip.net/bbs/search.php"
             />
-            <span class="input-group-btn">
-              <button type="submit" class="btn btn-sm">
-                <i class="fa fa-search fa-lg"></i>
-              </button>
-            </span>
-          </div>
-        </form>
+            <div class="input-group input-group-sm">
+              <input
+                type="text"
+                name="stx"
+                class="form-control input-sm"
+                value=""
+              />
+              <span class="input-group-btn">
+                <button type="submit" class="btn btn-sm">
+                  <i class="fa fa-search fa-lg"></i>
+                </button>
+              </span>
+            </div>
+          </form>
 
-        <div class="header-keyword">
-          <div class="basic-keyword">
-            <span class="stx">
-              <a href="https://gezip.net/bbs/search.php?stx="></a>
-            </span>
+          <div class="header-keyword">
+            <div class="basic-keyword">
+              <span class="stx">
+                <a href="https://gezip.net/bbs/search.php?stx="></a>
+              </span>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="clearfix"></div>
-    </header>
-  </div>
+        <div class="clearfix"></div>
+      </header>
+    </div>
+  </b-container>
 </template>
 
 <script>
